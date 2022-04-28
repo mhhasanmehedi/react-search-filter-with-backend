14f216b260d0359ff18997b25fc1e6a179445819
mhhasanmehedi <mmmehedi53@gmail.com>
2022-04-28 17:05:25 +0600

    first commit

---
+++ client/src/Components/Table.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+          <th>Gender</th>
+        </tr>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+            <td>{item.gender}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
